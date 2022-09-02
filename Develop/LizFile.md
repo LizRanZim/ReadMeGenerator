c22f94a6ccfc4c80f78359f550201dc28a6fb711
LizRanZim <lrzclass22@gmail.com>
2022-09-01 22:01:25 -0400

    Added badge functionality

--- Develop/LizFile.md
+++ Develop/LizFile.md
@@ -1,2 +1,6 @@
-# MyFile27
-    ## It creates stuff.
+# This is it
+    ## You are awesome
+
+[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
+    
+    
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,27 +1,41 @@
 
 
 const licenseChosen = {
-  Apache: '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  'Apache License v2.0': {
+    badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)',
+    link: '(https://opensource.org/licenses/Apache-2.0)',
+  },
 
-  GNU: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-// look at syntax so I'm specifying license
+  GNU: {
+    badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
+    link: '(https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  // look at syntax so I'm specifying license
 }
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-function renderLicenseBadge(license) { 
- 
+function renderLicenseBadge(license) {
+  if (license) {
+    return licenseChosen[license].badge
+  }
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license) {
+    return licenseChosen[license].link
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
@@ -46,7 +60,11 @@ function generateMarkdown(response) {
   // console.log(email);
 
   return `# ${response.title}
-    ## ${response.description}`
+    ## ${response.description}
+
+${renderLicenseBadge(response.license)}
+    
+    `
 
     ;
 }
