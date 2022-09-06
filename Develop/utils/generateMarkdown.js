16827657e959c3f003267b9809030279752e1293
LizRanZim <lrzclass22@gmail.com>
2022-09-06 16:31:39 -0400

    Fix sytnax in generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,7 +9,7 @@ const licenseChosen = {
   'GNU General Public License v3.0': {
     badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
     link: '(https://www.gnu.org/licenses/gpl-3.0)'
-  }
+  },
 
   'MIT License': {
     badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]',
